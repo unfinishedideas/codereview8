29d746b6dc90614e1c091547793619993b2dc2a6
Peter Wells <petethebeatwells@gmail.com>
2019-12-20 14:09:01 -0800

    def rspec: definition clear, == and save specs tested

--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -10,21 +10,41 @@ describe '#Definition' do
 
   describe('.all') do
     it('Shows all the definitions in @@definitions_list') do
-      def1 = Definition.new('Something shiny', nil)
-      def1.save
-      def2 = Definition.new('Is really old', nil)
-      def2.save
-      expect(Definition.all).to(eq([def1, def2]))
+      definition1 = Definition.new('Something shiny', nil)
+      definition1.save
+      definition2 = Definition.new('Is really old', nil)
+      definition2.save
+      expect(Definition.all).to(eq([definition1, definition2]))
     end
   end
 
   describe('#save') do
     it('should save a definition with a unique ID to @@definitions_list') do
-      def1 = Definition.new('Something shiny', nil)
-      def1.save
-      expect(Definition.all).to(eq([def1]))
+      definition1 = Definition.new('Something shiny', nil)
+      definition1.save
+      expect(Definition.all).to(eq([definition1]))
     end
   end
 
+  describe('#==') do
+    it('should overwrite == operator to compare definitions by text') do
+      definition1 = Definition.new('Something shiny', nil)
+      definition1.save
+      definition2 = Definition.new('Something shiny', nil)
+      definition2.save
+      expect(definition1).to(eq(definition2))
+    end
+  end
+
+  describe('.clear') do
+    it('should clear the list of definitions when called') do
+      definition1 = Definition.new('Something shiny', nil)
+      definition1.save
+      definition2 = Definition.new('Is really old', nil)
+      definition2.save
+      Definition.clear
+      expect(Definition.all).to(eq([]))
+    end
+  end
 
 end
