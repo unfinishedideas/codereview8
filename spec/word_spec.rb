f575c66f8969a313c72dd1927fe2e197d82d0357
Peter Wells <petethebeatwells@gmail.com>
2019-12-20 12:38:14 -0800

    rspec: testing to find words in @@words_list

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -17,25 +17,6 @@ describe '#Word' do
     end
   end
 
-  describe('.save') do
-    it('Saves a word to @@words_list class hash variable') do
-      word1 = Word.new("Zoombini", ["A strange fluffy creature with mismatched features"], nil)
-      word1.save
-      expect(Word.all).to(eq([word1]))
-    end
-  end
-
-  describe('.delete') do
-    it ('deletes a word from @@words_list') do
-      word1 = Word.new("Zoombini", ["A strange fluffy creature with mismatched features"], nil)
-      word1.save
-      word2 = Word.new("Cutenessvom", ["When something is so cute you vomit"], nil)
-      word2.save
-      word1.delete
-      expect(Word.all).to(eq([word2]))
-    end
-  end
-
   describe('#==') do
     it('Tests overwrite of == operator to match words by name') do
       word1 = Word.new("Zoombini", ["A strange fluffy creature with mismatched features"], nil)
@@ -57,6 +38,33 @@ describe '#Word' do
     end
   end
 
+  describe('.save') do
+    it('Saves a word to @@words_list class hash variable') do
+      word1 = Word.new("Zoombini", ["A strange fluffy creature with mismatched features"], nil)
+      word1.save
+      expect(Word.all).to(eq([word1]))
+    end
+  end
 
+  describe('.delete') do
+    it ('deletes a word from @@words_list') do
+      word1 = Word.new("Zoombini", ["A strange fluffy creature with mismatched features"], nil)
+      word1.save
+      word2 = Word.new("Cutenessvom", ["When something is so cute you vomit"], nil)
+      word2.save
+      word1.delete
+      expect(Word.all).to(eq([word2]))
+    end
+  end
+
+  describe('.find') do
+    it ('finds a word from @@words_list') do
+      word1 = Word.new("Zoombini", ["A strange fluffy creature with mismatched features"], nil)
+      word1.save
+      word2 = Word.new("Cutenessvom", ["When something is so cute you vomit"], nil)
+      word2.save
+      expect(Word.find(2)).to(eq(word2))
+    end
+  end
 
 end
