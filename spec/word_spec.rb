cbd122f30a9e7ad22e6c5b600393202a41007c9a
Peter Wells <petethebeatwells@gmail.com>
2019-12-20 12:27:12 -0800

    rspec: Word test save method

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -11,9 +11,17 @@ describe '#Word' do
     it('Shows all the words') do
       word1 = Word.new("Zoombini", "A strange fluffy creature with mismatched features", nil)
       word1.save
-      word2 = Word.new("Cutenessvom", "When something is so cute you vomit", nil)
+      word2 = Word.new("Cutenessvom", ["When something is so cute you vomit"], nil)
       word2.save
       expect(Word.all).to(eq([word1, word2]))
+    end
+  end
+
+  describe('.save') do
+    it('Saves a word to @@words_list class hash variable') do
+      word1 = Word.new("Zoombini", "A strange fluffy creature with mismatched features", nil)
+      word1.save
+      expect(Word.all).to(eq([word1]))
     end
   end
 
